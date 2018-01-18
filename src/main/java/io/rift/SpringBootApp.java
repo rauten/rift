c6a96b96c5db2710276df86a4bd058f83723a062
Riley Auten <rileyyauten@gmail.com>
2018-01-18 14:12:48 -0500

    Initial setup

---
+++ src/main/java/io/rift/SpringBootApp.java
@@ -0,0 +1,16 @@
+package io.rift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class SpringBootApp {
+
+    public static void main(String[] args) throws Exception {
+        new SpringApplication(SpringBootApp.class).run(args);
+    }
+
+
+}
