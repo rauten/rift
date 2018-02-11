7e77bf7e0026b1f14e0fdf7821dab8f7dddab615
Riley Auten <rileyyauten@gmail.com>
2018-02-11 14:27:02 -0500

    Update a session (with trigger to update slots_remaining for corresponding session)

--- src/main/java/io/rift/service/SessionRequestService.java
+++ src/main/java/io/rift/service/SessionRequestService.java
@@ -1,14 +1,23 @@
 package io.rift.service;
 
 
+import com.google.common.base.CaseFormat;
 import com.sun.org.apache.xpath.internal.operations.Bool;
 import io.rift.model.RifterSession;
 import io.rift.model.SessionRequest;
 import io.rift.model.Usertable;
 import io.rift.repository.RiftRepository;
+import org.apache.bcel.classfile.ClassParser;
+import org.apache.bcel.classfile.Field;
+import org.apache.bcel.classfile.JavaClass;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -26,8 +35,20 @@ public class SessionRequestService {
     @Autowired
     private RiftRepository riftRepository;
 
+    private final Integer POPULATESIZE = 4;
+
     private final String getGameRequestBySessionAndRifteeId = "getGameRequestBySessionAndRifteeId";
     private final String createSessionRequest = "createSessionRequest";
+
+    private final String updateSessionRequestStart = "UPDATE gameRequest SET (";
+    private final String updateSessionRequestPath = "/io/rift/model/SessionRequest.class";
+    private final String sessionRequestClass = "SessionRequest.class";
+    private final String updateSessionRequestEnd = " WHERE riftee_id = ? AND session_id = ?";
+
+    private final String booleanStr = "Boolean";
+    private final String stringStr = "String";
+    private final String intStr = "Integer";
+    private final String doubleStr = "Double";
 
     public SessionRequest populateGameRequest(ResultSet resultSet, int startPoint) throws SQLException {
         SessionRequest sessionRequest = new SessionRequest();
@@ -88,11 +109,57 @@ public class SessionRequestService {
         return false;
     }
 
-    /*
-    public Boolean acceptRequest(SessionRequest sessionRequest) throws SQLException {
+    public Boolean updateSessionRequest(SessionRequest sessionRequest) throws SQLException, IOException, IntrospectionException, IllegalAccessException, InvocationTargetException {
+        int rifteeId = sessionRequest.getRifteeId();
+        int sessionId = sessionRequest.getSessionId();
+        String str = updateSessionRequestStart;
+        StringBuilder query = new StringBuilder(str);
+        StringBuilder values = new StringBuilder("(");
+        ClassParser parser = new ClassParser(UsertableService.class.getResourceAsStream(updateSessionRequestPath), sessionRequestClass);
+        JavaClass javaClass = parser.parse();
+        Field[] fields = javaClass.getFields();
+        List<Object> args = new ArrayList<>();
+        boolean didAdd = false;
+        for (int i = 0; i < POPULATESIZE; i++) {
+            String[] properties = fields[i].toString().split(" ");
+            String attribute = properties[2];
+            PropertyDescriptor pd = new PropertyDescriptor(attribute, SessionRequest.class);
+            Method getter = pd.getReadMethod();
+            Object f = getter.invoke(sessionRequest);
+            if (f != null && !(f.toString()).equals("")) {
+                didAdd = true;
+                if (properties[1].equals(intStr)) {
+                    f = (Integer) f;
+                } else if (properties[1].equals(stringStr)) {
+                    f = (String) f;
+                } else if (properties[1].equals(doubleStr)) {
+                    f = (Double) f;
+                } else if (properties[2].equals(booleanStr)) {
+                    f = (boolean) f;
+                }
+                attribute = CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, attribute);
+                query.append(attribute);
+                query.append(", ");
+                values.append("?");
+                values.append(", ");
+                args.add(f);
+            }
+        }
+        if (didAdd) {
+            query.delete(query.length() - 2, query.length() - 1);
+            values.delete(values.length() - 2, query.length() - 1);
+            query.append(") = ");
+            values.append(")");
+            query.append(values);
+            query.append(updateSessionRequestEnd);
+            args.add(rifteeId);
+            args.add(sessionId);
+            return riftRepository.doUpdate(query, args);
+        }
+        return true;
 
     }
-    */
+
 
 
 }
