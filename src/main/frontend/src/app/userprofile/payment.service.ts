4f9e5b3b3b56f24cee9ce28ecdeb8e38553b7a12
gordonyli <gordonyli@gmail.com>
2018-03-01 13:09:07 -0500

    added payment service.ts

---
+++ src/main/frontend/src/app/userprofile/payment.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { Userprofile } from "../models/userprofile";
+import {Http, RequestOptions, Response, Headers} from "@angular/http";
+import { Observable } from "rxjs/Observable";
+import "rxjs/Rx";
+
+
+
+@Injectable()
+export class UserprofileService {
+  private createUserURL = "/api/user/createUser";
+  private uploadProfilePictureURL = "/api/user/putPicture/";
+  private getProfilePictureURL = "/api/user/getPicture/";
+
+  constructor(private http: Http) {
+  }
+
+  getUser(riftTag: string): Observable<Userprofile> {
+    console.log("running getUser");
+    return this.http.get("/api/user/" + riftTag + "/profilePage")
+      .map(
+        (response: Response) => {
+          return response.json();
+        }
+      )
+      .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+  }
+
+
+  createUser(data): void {
+    console.log("running createUser");
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    this.http.put(this.createUserURL, data, options)
+      .map(res => res.json())
+      .catch((error:any) => Observable.throw(error.json().error || 'Serve error'))
+      .subscribe();
+  }
+
+}
