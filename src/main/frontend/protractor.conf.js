9dab6ba1c5d752a3273bdf7cfe36e55b203ba495
gordonyli <gordonyli@gmail.com>
2018-03-27 16:35:11 -0400

    First Commit

--- src/main/frontend/protractor.conf.js
+++ src/main/frontend/protractor.conf.js
@@ -20,6 +20,7 @@ exports.config = {
     print: function() {}
   },
   onPrepare() {
+    // noinspection JSAnnotator
     require('ts-node').register({
       project: 'e2e/tsconfig.e2e.json'
     });
