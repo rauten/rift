4ff9da46a9af6fdaf2b08983c46bed8327af70e9
gordonyli <gordonyli@gmail.com>
2018-03-28 10:22:40 -0400

    viewcontroller

--- src/main/java/io/rift/controller/ViewController.java
+++ src/main/java/io/rift/controller/ViewController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ViewController {
 
     @RequestMapping(
-            { "/home", "/user/{rifttag:\\w+}", "/user/{rifttag:\\w+}/update"
+            { "/home", "/user/{rifttag:\\w+}", "/user/{rifttag:\\w+}/update",
                     "/location", "/about", "/tests","/tests/new",
                     "/tests/**","/questions","/answers" })
     public String index() {
